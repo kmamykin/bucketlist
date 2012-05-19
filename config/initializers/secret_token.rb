8cdbcae36173e65dc41cb29e24bbe3afde57f4a8
Kliment Mamykin <kmamyk@yahoo.com>
2012-05-19 16:10:40 -0400

    Initial commit rails 3.2.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bucketlist::Application.config.secret_token = '417e77e5bc19bfd81c23162fd2a87e2c4912a854a8d75f2cc8a1e6ab535a49695948f1cbcca4ebb0dad5b02374a10c21685cfe7ac3f63bfe7839d89b4d29bab0'
