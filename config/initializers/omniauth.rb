2515e1ef7f98ddc4f7bb956c06da57f29e42564c
Kliment Mamykin <kmamyk@yahoo.com>
2012-05-20 00:30:54 -0400

    Using ENV for app keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :twitter, 'lQ9sCHktasDn47QK2oZXg', 'NhV0BkkK4u3tD1zeNoHnPI4bOBOmNpip7Wdw6aMgnY' # production
-  provider :twitter, 'DtsaSGt35L6npnjMsQgw', '2lRyt2B99kI1PHnc0SFrFCqWPkd7GKweFvPRP9EiX0A'
-  #provider :facebook, '390672117650826', 'd6cd06cab547c791223c604fade27d80' # bucketlist.travel
-  provider :facebook, '389644724419703', '6ffecf566915f6baeffece32ecb62f6d' # bucketlist.dev
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
