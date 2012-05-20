9427f8f724d5cbda578f966aad117d21d1871d6b
Kliment Mamykin <kmamyk@yahoo.com>
2012-05-20 05:21:31 -0400

    editor binds directly by id

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review
   include Mongoid::Document
+  include Mongoid::Paperclip
   include Mongo::Voteable
 
   embedded_in :experience
@@ -7,6 +8,9 @@ class Review
   voteable Experience, :up => +2, :down => -1
 
   belongs_to :user
-  field :title, type: String
   field :text, type: String
+  has_mongoid_attached_file :image,
+                            :storage        => :s3,
+                            :s3_credentials => {access_key_id: ENV['S3_ACCESS_KEY_ID'], secret_access_key: ENV['S3_SECRET_ACCESS_KEY']},
+                            :bucket         => 'uploads-bucketlist'
 end
