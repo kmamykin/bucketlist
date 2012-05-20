3e5aab4e018123b79cb28ffd24bac7bd975bcf68
Kliment Mamykin <kmamyk@yahoo.com>
2012-05-19 21:49:33 -0400

    Storing images on S3

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,7 +3,10 @@ class Experience
   include Mongoid::Timestamps
   include Mongoid::Paperclip
 
-  has_mongoid_attached_file :image
+  has_mongoid_attached_file :image,
+                            :storage        => :s3,
+                            :s3_credentials => {access_key_id: ENV['S3_ACCESS_KEY_ID'], secret_access_key: ENV['S3_SECRET_ACCESS_KEY']},
+                            :bucket => 'uploads-bucketlist'
   field :title, type: String
   field :description, type: String
 
